978a6038207f7f642e081e7575750142322604db
GaneshaBharadwaj <ganeshhnhedse96@gmail.com>
2023-05-01 17:01:38 +0530

    testing introduction

---
+++ Testng-Automation/src/com/sgtesting/users/Users.java
@@ -0,0 +1,36 @@
+package com.sgtesting.users;
+
+public class Users {
+	
+	
+	@Test
+	public void createUser()
+	{
+		System.out.println("The User demoUser1 has created successfully");
+	}
+
+	@Test
+	public void modifyUser()
+	{
+		System.out.println("The User demoUser1 has modified successfully");
+	}
+	
+	@Test
+	public void deleteUser()
+	{
+		System.out.println("The User demoUser1 has deleted successfully");
+	}
+	
+	@BeforeClass
+	public void setup()
+	{
+		System.out.println("Launch Browser, Navigate URL and Login into the Application");
+	}
+	
+	@AfterClass
+	public void tearDown()
+	{
+		System.out.println("Logout from the Application and Close the Application");
+		System.out.println("+++++++++++++++++++++++++++");
+	}
+}
